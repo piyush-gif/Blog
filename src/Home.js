ca49344976c84ec8e6a4495efde20e3f08fc9a2d
piyush <piyush.khadka2233@gmail.com>
2025-05-15 19:54:58 +0100

    click events added

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
 const Home = () => {
+  const handleClick = (e) =>{
+    console.log('hello, ninja', e)
+  }
+  const handleClicAgain =(name)=> {
+    console.log('hello'+ name);
+  }
+
   return ( 
     <div className="home">
       <h2>HomePage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={()=>{handleClicAgain('mario')}}>click me again</button>
     </div>
    );
 }
