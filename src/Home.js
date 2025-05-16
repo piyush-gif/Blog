31052b28664659e9c0c010635ac71269a6745a63
piyush <piyush.khadka2233@gmail.com>
2025-05-16 14:37:37 +0100

    conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -3,24 +3,24 @@ import BlogList from "./BlogLIst";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-
-  const [name, setName] = useState('mario');
-
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-    .then(res => {
-      return res.json()
-    })
-    .then(data => {
-      setBlogs(data);
-    });
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        setBlogs(data);
+        setIsPending(false);
+      });
+    }, 1000)
   },[]);
   return ( 
     <div className="home">
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All blogs!" />}
-      <button onClick={()=> setName('luigi')}>change name</button>
-      <p>{name}</p>
     </div>
    );
 }
