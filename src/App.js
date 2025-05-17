50295b85f0f962762279a85d5f9a9e3328e15139
piyush <piyush.khadka2233@gmail.com>
2025-05-17 16:55:10 +0100

    Route parameters used

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
+import BlogDetails from './BLogDetails';
 
 function App() {
-
   return (
     <Router>
       <div className="App">
@@ -16,6 +16,9 @@ function App() {
             </Route>
             <Route path="/create"> 
               <Create />
+            </Route>
+            <Route path="/blogs/:id"> 
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
