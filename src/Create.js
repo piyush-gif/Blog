6ff28beb377e26e32f832e6be2721411a499e134
piyush <piyush.khadka2233@gmail.com>
2025-05-17 22:28:35 +0100

    post request to the database

--- src/Create.js
+++ src/Create.js
@@ -4,10 +4,25 @@ const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
+
+  const handleSubmit=(e) => {
+    e.preventDefault();
+    const blog= {title, body, author};
+    setIsPending(true);
+    fetch('http://localhost:8000/blogs',{
+      method:'POST',
+      headers: {'Content-type': "application/json"},
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added');
+      setIsPending(false);
+    })
+  }
   return (
    <div className="create">
     <h2>Add a new blog</h2>
-    <form >
+    <form onSubmit={handleSubmit}>
       <label>Blog title</label>
       <input 
       type="text"
@@ -17,7 +32,7 @@ const Create = () => {
       />
       <label>Blog body:</label>
       <textarea
-      requried
+      required
       value= {body}
       onChange={(e) => setBody(e.target.value)}
       ></textarea>
@@ -29,10 +44,8 @@ const Create = () => {
         <option value="mario">mario</option>
         <option value="yoshi">yoshi</option>
       </select>
-      <button>Add blog</button>
-      <p>{title}</p>
-      <p>{body}</p>
-      <p>{author}</p>
+      {!isPending && <button >Add blog</button>}
+      {isPending && <button disabled>Adding blog...</button>}
     </form>
    </div>
   );
