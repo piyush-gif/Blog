6e9448b406784e5bcd0cb55062aa8c03865717f0
piyush <piyush.khadka2233@gmail.com>
2026-01-08 13:07:42 +0000

    rename bloddetails

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './Navbar';
-import Home from './Home';
-import Create from './Create';
-import BlogDetails from './BLogDetails';
-import NotFound from './NotFound';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Create from "./Create";
+import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar/>
+        <Navbar />
         <div className="content">
           <Switch>
-            <Route exact path="/"> 
+            <Route exact path="/">
               <Home />
             </Route>
-            <Route path="/create"> 
+            <Route path="/create">
               <Create />
             </Route>
-            <Route path="/blogs/:id"> 
+            <Route path="/blogs/:id">
               <BlogDetails />
             </Route>
             <Route path="*">
-              <NotFound/>
+              <NotFound />
             </Route>
           </Switch>
         </div>
